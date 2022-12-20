42c21507bc6e769c9ae5a4d7bc34b8ea1ccf4b1e
Luan Carlos Araldi <luan@MacBook-Pro-de-Luan.local>
2022-12-20 14:23:27 -0300

    Linter on dag file

--- dags/twitter-pipeline.py
+++ dags/twitter-pipeline.py
@@ -1,4 +1,3 @@
-
 import pendulum
 import json
 
@@ -7,7 +6,8 @@ from airflow.decorators import task
 from airflow.models import Variable
 from datetime import datetime, timedelta
 import logging
-import pandas as pd
+
+now = pendulum.now("Europe/Berlin")
 
 with DAG(
     dag_id="twitter_pipeline",
@@ -15,22 +15,28 @@ with DAG(
     start_date=pendulum.datetime(2022, 12, 1, tz="UTC"),
     catchup=False,
     tags=["twitter"],
-    default_args={"retries": 5}
+    default_args={"retries": 5},
+    params={
+        "date_start": now.subtract(days=7),
+        "date_end": now,
+        "tweet_query": "flixbus",
+    },
 ) as dag:
 
-    now = pendulum.now("Europe/Berlin")
-
     @task(retries=3, retry_delay=timedelta(seconds=20))
-    def process_twitter_data(date_end: datetime = now, date_start: datetime = now.subtract(days=7)):
+    def process_twitter_data(
+        date_end: datetime = now, date_start: datetime = now.subtract(days=7)
+    ):
         import glob
         import pyarrow as pa
         import pyarrow.parquet as pq
+        import pandas as pd
 
         if date_start > date_end:
             logging.ERROR("End date bigger than start date, not able to process data")
 
-        parquet_partition_file_path = glob.glob('/tmp/data/raw/**/**/*')
-        
+        parquet_partition_file_path = glob.glob("/tmp/data/raw/**/**/*")
+
         date_to_find = date_start
 
         partitions_to_read_parquet_files_from = []
@@ -40,11 +46,11 @@ with DAG(
                 partitions_to_read_parquet_files_from.append(partition_file_string)
             date_to_find = date_to_find.add(days=1)
 
-        
         df = pd.concat(
-            pd.read_parquet(parquet_file) for parquet_file in partitions_to_read_parquet_files_from
+            pd.read_parquet(parquet_file)
+            for parquet_file in partitions_to_read_parquet_files_from
         ).drop_duplicates(subset=["text", "author_id", "created_at"])
-        
+
         def check_if_is_retweet(row):
             if row is None:
                 return False
@@ -55,37 +61,41 @@ with DAG(
 
         df2 = pd.DataFrame().assign(
             text=df["text"],
-            user_id=df["author_id"], 
-            location=df["author.location"], 
+            user_id=df["author_id"],
+            location=df["author.location"],
             number_of_followers=df["author.public_metrics.followers_count"],
             created_at=df["created_at"],
             hashtags=df["entities.hashtags"],
             retweet_count=df["public_metrics.retweet_count"],
             is_retweet=df["referenced_tweets"],
-            partition_date=df["created_at"].dt.to_period('D').astype(str))
+            partition_date=df["created_at"].dt.to_period("D").astype(str),
+        )
 
-        df2["is_retweet"] = df2.apply(lambda row: check_if_is_retweet(row["is_retweet"]), axis=1)
+        df2["is_retweet"] = df2.apply(
+            lambda row: check_if_is_retweet(row["is_retweet"]), axis=1
+        )
 
         table = pa.Table.from_pandas(df2)
 
         logging.info("Writing tweets to parquet file")
         pq.write_to_dataset(
-            table,
-            root_path='/tmp/data/processed',
-            partition_cols=['partition_date']
+            table, root_path="/tmp/data/processed", partition_cols=["partition_date"]
         )
 
-
-
-
     @task(retries=3, retry_delay=timedelta(seconds=20))
-    def get_twitter_data_by_mention_and_date_range(mentions: str = "flixbus", date_end: datetime = now, date_start: datetime = now.subtract(days=7)):
+    def get_twitter_data_by_mention_and_date_range(
+        mentions: str = "flixbus",
+        date_end: datetime = now,
+        date_start: datetime = now.subtract(days=7),
+    ):
         from twarc import Twarc2, expansions
         from flatdict import FlatDict
         import pyarrow as pa
         import pyarrow.parquet as pq
+        import pandas as pd
+
         logging.info("Starting to fetch data from Twitter API")
-        client = Twarc2(bearer_token=Variable.get('twitter_bearer_token'))
+        client = Twarc2(bearer_token=Variable.get("twitter_bearer_token"))
         # End time shenanigans for twitter api without privileged access, end time needs to be 10s before the request time
         end_time = date_end.subtract(seconds=10)
         # Workaroud to get around the non privileged access of getting a 401 after a couple of requests because the start time was bigger than 7 days
@@ -97,21 +107,26 @@ with DAG(
         media_fields = "public_metrics"
         query = mentions
 
-        logging.info(f"Starting to get tweets with query {query} from {start_time} to {end_time}...")
+        logging.info(
+            f"Starting to get tweets with query {query} from {start_time} to {end_time}..."
+        )
 
         tweets_page = client.search_recent(
-            query=query, 
-            start_time=start_time, 
-            end_time=end_time, 
+            query=query,
+            start_time=start_time,
+            end_time=end_time,
             expansions=expansion_fields,
             tweet_fields=tweet_fields,
             user_fields=user_fields,
             media_fields=media_fields,
-            max_results=100
+            max_results=100,
         )
 
         for page in tweets_page:
-            tweets = [dict(FlatDict(tweet, delimiter=".")) for tweet in expansions.flatten(page)]
+            tweets = [
+                dict(FlatDict(tweet, delimiter="."))
+                for tweet in expansions.flatten(page)
+            ]
 
             for tweet in tweets:
                 if tweet.get("referenced_tweets", None):
@@ -121,20 +136,25 @@ with DAG(
 
             df = pd.DataFrame.from_dict(tweets)
 
-            #Partition by year,month,day column insertion
-            df['created_at'] = pd.to_datetime(df['created_at'])
-            df['year'] = df['created_at'].dt.year
-            df['month'] = df['created_at'].dt.month
-            df['day'] = df['created_at'].dt.day
+            # Partition by year,month,day column insertion
+            df["created_at"] = pd.to_datetime(df["created_at"])
+            df["year"] = df["created_at"].dt.year
+            df["month"] = df["created_at"].dt.month
+            df["day"] = df["created_at"].dt.day
 
             table = pa.Table.from_pandas(df)
 
             logging.info("Writing tweets to parquet file")
             pq.write_to_dataset(
                 table,
-                root_path='/tmp/data/raw',
-                partition_cols=['year', 'month', 'day'],
+                root_path="/tmp/data/raw",
+                partition_cols=["year", "month", "day"],
             )
 
-
-    get_twitter_data_by_mention_and_date_range() >> process_twitter_data()
+    get_twitter_data_by_mention_and_date_range(
+        mentions="{{params.tweet_query}}",
+        date_end="{{params.date_end}}",
+        date_start="{{params.date_start}}",
+    ) >> process_twitter_data(
+        date_end="{{params.date_end}}", date_start="{{params.date_start}}"
+    )
